496251ea7896098e1f0f977a86f00b0332007036
Naseer Hussain <nhussain@CIBER-INDIA.IN>
2018-06-12 14:00:11 +0530

    logic & css added for the game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
   public correct:number;
   public total:number; 
-  public score:string; 
-  //arr = [[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]];
+  public score:number; 
   public arr:any;
 
   ngOnInit(){
     this.correct = 0;
     this.total = 0;
-    this.score="0";
+    this.score=0;
     this.arr = this.getArray();
   }
 
-  clicked(row,col,obj){
-    //console.log("Row = "+x+" Column = "+i);
-    
-    // for(let a=0;a<8;a++){
-    //   for(let b=0;b<8;b++){
-        console.log(obj);
+  clicked(row,col){
         let x = this.arr[row][col]
         if(x.diamond){
           this.correct += 1;
+          x.image1 = true;
+        }else{
+          x.image2 = true;
         }
+        this.arr[row][col] = x;
         this.total += 1;
-        this.score =((this.correct/this.total)*100).toFixed(2);
-    //  }
-  //  }
-    console.log("Correct=" + this.correct);
-    console.log("Total="+ this.total);
+        this.score = 64- this.total
   }
 
   getArray(){
@@ -42,7 +36,6 @@ export class AppComponent {
     for(let a=0;a<8;a++){
       arr.push(this.getRandom());
     }
-    //console.log(arr);
     return arr;
   }
 
@@ -54,12 +47,14 @@ export class AppComponent {
       if(i==rand){
         obj={
           diamond:true,
-          point:1
+          image1:false,
+          image2:false
         };
       }else{
         obj = {
           diamond:false,
-          point:0
+          image1:false,
+          image2:false
         };
       }
       arr.push(obj);
